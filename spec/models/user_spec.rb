2de48949e4958db88ccc026e92041291bd2ca573
Edy Dawson <edawson@oreilly.com>
2013-06-09 20:16:15 -0700

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,10 +15,10 @@ describe User do
 #  pending "add some examples to (or delete) #{__FILE__}"
 #end
 
-before  do
-	@user = User.new(name: "Example User", email: "user@example.com",
-		password: "foobar", password_confirmation: "foobar" )
-end
+before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
 subject { @user }
 
@@ -48,48 +48,48 @@ describe "when name is too long" do
 end
 
 describe "when email format is invalid" do
-	it "should be invalid" do
-		addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
-		addresses.each do |invalid_address|
-			@user.email = invalid_address
-			@user.should_not be_valid
-		end
-	end
-end
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end      
+    end
+  end
 
 describe "when email format is valid" do
-	it "should be valid" do
-		addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-		addresses.each do |valid_address|
-			@user.email = valid_address
-			@user.should be_valid
-		end
-	end
-end
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end      
+    end
+  end
 
 describe "when email address is already taken" do
-	before do
-		user_with_same_email = @user.dup
-		user_with_same_email.email = @user.email.upcase
-		user_with_same_email.save
-	end
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.save
+    end
 
-	it { should_not be_valid }
-end
+    it { should_not be_valid }
+  end
 
 describe "when password is not present" do
-	before { @user.password = @user.password_confirmation = " " }
-	it { should_not be_valid }
+  before { @user.password = @user.password_confirmation = " " }
+  it { should_not be_valid }
 end
 
-describe "When password doesn't match confirmation" do
-	before { @user.password_confirmation = "mismatch" }
-	it { should_not be_valid }
+describe "when password doesn't match confirmation" do
+  before { @user.password_confirmation = "mismatch" }
+  it { should_not be_valid }
 end
 
 describe "when password confirmation is nil" do
-	before { @user.password_confirmation = nil }
-	it { should_not be_valid }
+  before { @user.password_confirmation = nil }
+  it { should_not be_valid }
 end
 
 describe "return value of authenticate method" do
@@ -109,25 +109,24 @@ describe "return value of authenticate method" do
 end
 
 describe "with a password that's too short" do
-	before { @user.password = @user.password_confirmation = "a" * 5 }
-	it { should be_valid }
+  before { @user.password = @user.password_confirmation = "a" * 5 }
+  it { should be_invalid }
 end
 
 describe "return value of authenticate method" do
-	before { @user.save }
-	let(:found_user) { User.find_by_email(@user.email) }
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
 
-	describe "with valid password" do
-		it { should == found_user.authenticate(@user.password) }
-	end
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
 
-	describe "with invalid password" do
-		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-		it { should_not == user_for_invalid_password }
-		specify { user_for_invalid_password.should be_false }
-	end
-end
-
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
 
 end

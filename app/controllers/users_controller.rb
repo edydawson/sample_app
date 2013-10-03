0f485f49a462c912d39a39c9cbb65b25c734c4a2
Edy Dawson <edawson@oreilly.com>
2013-10-02 21:28:14 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
-#  before_filter :signed_in_user, only: [:index, :edit, :update]
-###  before_action :signed_in_user, only: [:index, :edit, :update]
-###  before_action :correct_user,   only: [:index, :edit, :update]
-  before_filter :signed_in_user, only: [:index, :edit, :update]
-  before_filter :correct_user,   only: [:index, :edit, :update]
+
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -30,13 +35,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-###    @user = User.find(params[:id])  This can come out
+     @user = User.find(params[:id])  
   end
 
   def update
-###    @user = User.find(params[:id])
-###    if @user.update_attributes(params[:user])
-    if @user.update_attributes(user_params)
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+
       flash[:success] = "Profile updated"
       sign_in @user
       redirect_to @user
@@ -55,14 +60,18 @@ class UsersController < ApplicationController
     # Before filters
 
     def signed_in_user
-##      unless signed_in?
-##      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
       end
     end
 
     def correct_user
-     @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
-#end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,11 +7,10 @@ describe "Authentication" do
 	describe "signin page" do
 		before { visit signin_path }
 
-		it { should have_selector('h1',		text: 'Sign in') }
+		it { should have_selector('h1',		  text: 'Sign in') }
 		it { should have_selector('title',	text: 'Sign in') }
 ####    it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 	end
-end
 
 describe "signin" do
   before { visit signin_path }
@@ -34,27 +33,28 @@ describe "signin" do
 
   describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
+####      before do
+####        fill_in "Email",    with: user.email.upcase
+####        fill_in "Password", with: user.password
+####        click_button "Sign in"
+####      end
+
       before { sign_in user }
 
+      it { should have_selector('title', text: user.name) }
 
-#      it { should have_selector('title', text: user.name) }
-#      it { should have_link('Profile',  href: user_path(user)) }
-#      it { should have_link('Sign out', href: signout_path) }
-#      it { should have_link('Settings', href: edit_user_path(user)) }
-#      it { should have_link('Users',    href: users_path) }
+      it { should have_link('Users',    href: users_path) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+
       it { should_not have_link('Sign in', href: signin_path) }
 
 ####      it { should have_title(user.name) }
 
-####      before do
-###        fill_in "Email",    with: user.email.upcase
-###        fill_in "Password", with: user.password
-###        click_button "Sign in"
-##      end
-
       describe "followed by signout" do
         before { click_link "Sign out" }
- #      it { should have_link('Sign in') }
+       it { should have_link('Sign in') }
      end
    end
 
@@ -77,73 +77,83 @@ describe "AuthenticationPages" do
   describe "GET /authentication_pages" do
 ####    it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-####      get authentication_pages_index_path
-###      response.status.should be(200)
+#      get authentication_pages_index_path
+#      response.status.should be(200)
     end
   end
 #end
 
 describe "authorization" do
 
-  describe "for non-signed-in users" do
-    let(:user) { FactoryGirl.create(:user) }
 
-    describe "when attempting to visit a protected page" do
-      before do
-        visit edit_user_path(user)
-        fill_in "Email", with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+  describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
 
-      describe "after signing in" do
+      before { sign_in non_admin }
 
-####        it "should render the desired protected page" do
-###          expect(page).to have_title('Edit user')
-        end
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_url) }
       end
     end
+
+  describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end
+      end
 
     describe "in the Users controller" do
 
-      describe "visiting the user index" do
-        before { visit users_path }
-#        it { should have_title('Sign in') }
-      end
-    end
-
       describe "visiting the edit page" do
-        before { visit edit_user_path(user) }
-#        it { should have_title('Sign in')}
-#        it { should have_selector('title', text: 'Sign in') }
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
       end
 
       describe "submitting to the update action" do
-        before { put user_path(user) }
-#        specify { response.should redirect_to(signin_path) }
-           before { patch user_path(user) }
-#           specify { expect(response).to redirect_to(signin_path) }
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+      end
+
+      describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
       end
 
   describe "as wrong user" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-    before {sign_in user, no_capybara: true }
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
 
-    describe "visiting Users#edit page" do
-      before { visit edit_user_path(wrong_user) }
-#      it { should_not have_title(full_title('Edit user')) }
-    end
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
+      end
 
-    describe "submitting a PATCH request to the Users#update action" do
-      before { patch user_path(wrong_user) }
-#      specify { expect(response).to redirect_to(root_url) }
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_url) }
+      end
     end
   end
+end
+end
 
-    end
-#  end
-#end
 
 
 
